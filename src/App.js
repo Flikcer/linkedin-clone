07cffaefbda057facff0a2415ebb066fd635373a
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-16 22:16:42 -0400

    layout for linkedin clone

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
