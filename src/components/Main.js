d5bd59bebbe6597e985204922c5517a45aef2e79
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-18 12:57:39 -0400

    added layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,70 @@
 import styled from "styled-components";
 
 const Main = (props) => {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" alt="user"></img>
+          <button>Share a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-i.svg" alt="icon"></img>
+            <span>Photo</span>
+          </button>
+
+          <button>
+            <img src="/images/video-icon.svg" alt="video" />
+            <span>Video</span>
+          </button>
+
+          <button>
+            <img src="/images/event-icon.svg" alt="event" />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <img src="/images/article-icon.svg" alt="article" />
+            <span>Write Article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: bottom;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: rgba(0, 0, 0, 0.2);
+  margin: 0 0 0 8px;
+  background-color: #fff;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      min-height: 48px;
+      background-color: transparent;
+    }
+  }
 `;
 
 export default Main;
--- src/components/home.js
+++ src/components/home.js
@@ -66,10 +66,10 @@ const Section = styled.section`
 const Layout = styled.div`
   display: grid;
   grid-template-areas: "leftside main rightside";
-  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300, 7fr);
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
   column-gap: 25px;
   row-gap: 25px;
-  margin: 25px;
+  margin: 25px 0;
   @media (max-width: 768px) {
     display: flex;
     flex-direction: column;
